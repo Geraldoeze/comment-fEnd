7d48b320cdcc9bb047880190b4f6bfca5ff1a815
Gerald Ezenagu <egerald344@gmail.com>
2023-03-07 00:03:27 +0100

    API

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ module.exports = {
   reactStrictMode: true,
   env: {
     CI: false,
-    BACKEND_URL: "http://localhost:5000/api"
+    BACKEND_URL: "https://comments-nine.vercel.app/api"
   }
 }
+
+
+
+//   env: {
+//     CI: false,
+//     BACKEND_URL: "http://localhost:5000/api"
+//   }
--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ function Homepage({ currentUser, replies, comments }) {
 
 export default Homepage;
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const comments = await axios({
     url: `${process.env.BACKEND_URL}/com`,
     method: "GET",
